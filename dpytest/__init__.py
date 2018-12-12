0fc378ef93ecba5a4e12c0652b6f49398e27869c
CraftSpider <runetynan@gmail.com>
2018-12-11 19:42:27 -0500

    Uploaded to pytest as 0.0.2, now in alpha mode after that version

--- dpytest/__init__.py
+++ dpytest/__init__.py
@@ -3,7 +3,7 @@ __title__ = "dpytest"
 __author__ = "Rune Tynan"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018-2019 CraftSpider"
-__version__ = "0.0.1a"
+__version__ = "0.0.2a"
 
 from . import backend
 from .runner import *
