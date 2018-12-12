b1509d920208867f1a6266b4d487ea5fe90c961c
CraftSpider <runetynan@gmail.com>
2018-12-11 19:37:34 -0500

    Setup to auto-update on commits, add data to __init__

--- dpytest/__init__.py
+++ dpytest/__init__.py
@@ -1,3 +1,9 @@
 
+__title__ = "dpytest"
+__author__ = "Rune Tynan"
+__license__ = "MIT"
+__copyright__ = "Copyright 2018-2019 CraftSpider"
+__version__ = "0.0.1a"
+
 from . import backend
 from .runner import *
--- setup.py
+++ setup.py
@@ -1,12 +1,35 @@
 
 import setuptools
+import re
+
+with open("dpytest/__init__.py", "r") as file:
+    try:
+        version = re.search(r"^__version__\s*=\s*[\"']([^\"']*)[\"']", file.read(), re.MULTILINE).group(1)
+    except Exception as e:
+        raise RuntimeError("Version isn't set")
+
+if version.endswith(("a", "b")):
+    try:
+        import subprocess as sp
+        p = sp.Popen(["git", "rev-list", "--count", "HEAD"], stdout=sp.PIPE, stderr=sp.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+        p = sp.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=sp.PIPE, stderr=sp.PIPE)
+        out, err = p.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except Exception as e:
+        raise RuntimeError("Failure to get current git commit")
 
 with open("README.md", "r") as file:
     long_description = file.read()
 
+print(version)
+
 setuptools.setup(
     name="dpytest",
-    version="0.0.1",
+    version=version,
     author="Rune Tynan",
     author_email="runetynan@gmail.com",
     description="A package that assists in writing tests for discord.py",
@@ -18,7 +41,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-        "Development Status :: 1 - Planning",
+        "Development Status :: 2 - Pre-Alpha",
         "Topic :: Software Development :: Testing"
     ]
 )
