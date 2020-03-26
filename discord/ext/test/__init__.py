7e19bcd8f977a3fb51d48c73a8b927048388c59b
Rune Tynan <runetynan@gmail.com>
2020-03-26 14:43:40 -0400

    Increment version

--- discord/ext/test/__init__.py
+++ discord/ext/test/__init__.py
@@ -3,7 +3,7 @@ __title__ = "dpytest"
 __author__ = "Rune Tynan"
 __license__ = "MIT"
 __copyright__ = "Copyright 2018-2019 CraftSpider"
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 from . import backend
 from .runner import *
