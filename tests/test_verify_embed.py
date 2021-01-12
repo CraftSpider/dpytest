4f717ba7141d0c179f766e8c0c70e74d2df9dfc3
Sergeileduc <sergei.leduc@gmail.com>
2021-01-12 18:39:17 +0100

    new test : verify_embed KO

--- tests/test_verify_embed.py
+++ tests/test_verify_embed.py
@@ -20,6 +20,22 @@ async def test_embed(bot):
 
 
 @pytest.mark.asyncio
+async def test_embed_KO(bot):
+    guild = bot.guilds[0]
+    channel = guild.text_channels[0]
+
+    embed = discord.Embed(title="Test Embed")
+    embed.add_field(name="Field 1", value="Lorem ipsum")
+    
+    embed2 = discord.Embed(title="Test Embed KO")
+    embed2.add_field(name="Field 35", value="Foo Bar")
+
+    await channel.send(embed=embed)
+    dpytest.verify_embed(embed2, equals=False)
+    await dpytest.empty_queue()
+
+
+@pytest.mark.asyncio
 async def test_embed_assert_nothing(bot):
 
     dpytest.verify_embed(assert_nothing=True)
