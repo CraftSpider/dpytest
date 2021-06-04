dd8ad7edf2a975e49abb1fdc221bbcab812d46f6
Niels van der Vegt <ns.vandervegt@gmail.com>
2021-06-05 00:34:31 +0200

    Add bot client as a guild member

--- discord/ext/test/runner.py
+++ discord/ext/test/runner.py
@@ -570,6 +570,8 @@ def configure(client: discord.Client, num_guilds: int = 1, num_channels: int = 1
             user = back.make_user(f"TestUser{str(num)}", f"{num+1:04}")
             member = back.make_member(user, guild, nick=user.name + f"_{str(num)}_nick")
             members.append(member)
+        client_member = back.make_member(client.user, guild, nick=client.user.name + f"_nick")
+        members.append(client_member)
         back.make_member(back.get_state().user, guild)
 
     back.get_state().start_dispatch()
