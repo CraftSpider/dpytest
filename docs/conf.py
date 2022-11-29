efe1d914a48105d0de9c26e6243fce57caaffaac
Sergeileduc <sergei.leduc@gmail.com>
2022-11-29 17:51:29 +0100

    Bump version: 0.5.3 → 0.6.0

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, CraftSpider'
 author = 'CraftSpider'
 
 # The full version, including alpha/beta/rc tags
-release = '0.5.3'
+release = '0.6.0'
 
 
 # -- General configuration ---------------------------------------------------
