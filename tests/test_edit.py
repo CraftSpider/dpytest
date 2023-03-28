43b83e6c64ad3ddada55213c1b41f6b55f3768d1
JayDwee <drapj002@gmail.com>
2023-03-28 21:04:19 +0100

    test: add unit test for persisted message

--- tests/test_edit.py
+++ tests/test_edit.py
@@ -9,6 +9,10 @@ async def test_edit(bot):
     channel = guild.channels[0]
 
     mes = await channel.send("Test Message")
+    persisted_mes1 = await channel.fetch_message(mes.id)
     edited_mes = await mes.edit(content="New Message")
-
+    persisted_mes2 = await channel.fetch_message(mes.id)
+    
     assert edited_mes.content == "New Message"
+    assert persisted_mes1.content == "Test Message"
+    assert persisted_mes2.content == "New Message"
