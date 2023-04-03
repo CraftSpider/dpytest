d5dbd637771ab718dc4174e80bc7437aa175d9b6
Christian <59421913+Cikmo@users.noreply.github.com>
2023-04-03 11:14:33 +0200

    Refactor imports in __init__.py (#101)
    Refactor imports to use explicit exports as per PEP 484.

--- discord/ext/test/__init__.py
+++ discord/ext/test/__init__.py
@@ -5,7 +5,16 @@ __license__ = "MIT"
 __copyright__ = "Copyright 2018-2019 CraftSpider"
 __version__ = "0.6.3"
 
-from . import backend
+from . import backend as backend
+
 from .runner import *
-from .utils import embed_eq, activity_eq, embed_proxy_eq, PeekableQueue
-from .verify import verify, Verify, VerifyMessage, VerifyActivity
+
+from .utils import embed_eq as embed_eq
+from .utils import activity_eq as activity_eq
+from .utils import embed_proxy_eq as embed_proxy_eq
+from .utils import PeekableQueue as PeekableQueue
+
+from .verify import verify as verify
+from .verify import Verify as Verify
+from .verify import VerifyMessage as VerifyMessage
+from .verify import VerifyActivity as VerifyActivity
